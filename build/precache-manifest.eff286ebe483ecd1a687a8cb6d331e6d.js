ad9525c83556c60a5b4174a25a6886990443a70e
Ayush kumar <82118453+Ayush-ken14@users.noreply.github.com>
2022-07-27 17:23:37 +0530

    Add files via upload

---
+++ build/precache-manifest.eff286ebe483ecd1a687a8cb6d331e6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd535edb39f1ab38f6f18b8f9b70240d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec9a988bb1ce50b6ac8e",
+    "url": "/static/css/main.21a67fad.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9a988bb1ce50b6ac8e",
+    "url": "/static/js/main.5992215e.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
